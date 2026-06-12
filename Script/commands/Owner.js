491a364c18d04c4d1d93ea97d65c232763b45a04
MD SAKIL HASAN RUDRO <ishikadhor28@gmail.com>
2026-06-12 13:09:37 +0600

    Update Owner.js

--- Script/commands/Owner.js
+++ Script/commands/Owner.js
@@ -1,12 +1,13 @@
-const request = require("request");
 const fs = require("fs-extra");
+const axios = require("axios");
+const path = require("path");
 
 module.exports.config = {
   name: "owner",
-  version: "1.0.1",
+  version: "1.1.0",
   hasPermssion: 0,
-  credits: "SHAHADAT SAHU",
-  description: "Show Owner Info with styled box & random photo",
+  credits: "Rudro (fixed)",
+  description: "Show Owner Info with styled box & image",
   commandCategory: "Information",
   usages: "owner",
   cooldowns: 2
@@ -14,51 +15,61 @@ module.exports.config = {
 
 module.exports.run = async function ({ api, event }) {
 
-  
   const info = `
 ╔═════════════════════ ✿
 ║ ✨ 𝗢𝗪𝗡𝗘𝗥 𝗜𝗡𝗙𝗢 ✨
 ╠═════════════════════ ✿
-║ 👑 𝗡𝗮𝗺𝗲 : 𝗦𝗛𝗔𝗛𝗔𝗗𝗔𝗧 𝗦𝗔𝗛𝗨
-║ 🧸 𝗡𝗶𝗰𝗸 𝗡𝗮𝗺𝗲 : 𝗦𝗔𝗛𝗨
+║ 👑 𝗡𝗮𝗺𝗲 : 𝗥𝗨𝗗𝗥𝗢
 ║ 🎂 𝗔𝗴𝗲 : 𝟭𝟴+
 ║ 💘 𝗥𝗲𝗹𝗮𝘁𝗶𝗼𝗻 : 𝗦𝗶𝗻𝗴𝗹𝗲
-║ 🎓 𝗣𝗿𝗼𝗳𝗲𝘀𝘀𝗶𝗼𝗻 : 𝗦𝘁𝘂𝗱𝗲𝗻𝘁
-║ 📚 𝗘𝗱𝘂𝗰𝗮𝘁𝗶𝗼𝗻 : 𝗛𝗦𝗖
-║ 🏡 𝗔𝗱𝗱𝗿𝗲𝘀𝘀 : 𝗞𝗵𝗮𝗴𝗿𝗮𝗰𝗵𝗮𝗿𝗶
+║ 🎓 𝗘𝗱𝘂𝗰𝗮𝘁𝗶𝗼𝗻 : 𝐌𝐁𝐀
+║ 🏡 𝗔𝗱𝗱𝗿𝗲𝘀𝘀 : 𝐍𝐚𝐫𝐚𝐲𝐚𝐧𝐠𝐚𝐧𝐣
 ╠═════════════════════ ✿
-║ 🔗 𝗖𝗢𝗡𝗧𝗔𝗖𝗧 𝗟𝗜𝗡𝗞𝗦
-╠═════════════════════ ✿
-║ 📘 𝗙𝗮𝗰𝗲𝗯𝗼𝗼𝗸 :
-║ fb.com/100001039692046
-║ 💬 𝗠𝗲𝘀𝘀𝗲𝗻𝗴𝗲𝗿 :
-║ m.me/100001039692046
-║ 📞 𝗪𝗵𝗮𝘁𝘀𝗔𝗽𝗽 :
-║ wa.me/01882333052
-║ ✈️ 𝗧𝗲𝗹𝗲𝗴𝗿𝗮𝗺 :
-║ t.me/yoursahu
+║ 📘 Facebook:
+║ https://www.facebook.com/61571107303187
 ╚═════════════════════ ✿
 `;
 
+  // ✔ FIX: valid direct image only
   const images = [
-    "https://i.imgur.com/8WBso8x.png",
-    "https://i.imgur.com/0VZu5eY.png",
-    "https://i.imgur.com/bkixgPK.jpeg",
-    "https://i.imgur.com/z6G6L4c.jpeg"
+    "https://i.imgur.com/ZWLgcJl.png"
   ];
 
   const randomImg = images[Math.floor(Math.random() * images.length)];
 
-  const callback = () => api.sendMessage(
-    {
-      body: info,
-      attachment: fs.createReadStream(__dirname + "/cache/owner.jpg")
-    },
-    event.threadID,
-    () => fs.unlinkSync(__dirname + "/cache/owner.jpg")
-  );
+  const cacheDir = path.join(__dirname, "cache");
+  fs.ensureDirSync(cacheDir);
 
-  return request(encodeURI(randomImg))
-    .pipe(fs.createWriteStream(__dirname + "/cache/owner.jpg"))
-    .on("close", () => callback());
+  const imgPath = path.join(cacheDir, "owner.jpg");
+
+  try {
+    const writer = fs.createWriteStream(imgPath);
+
+    const response = await axios({
+      url: randomImg,
+      method: "GET",
+      responseType: "stream"
+    });
+
+    response.data.pipe(writer);
+
+    writer.on("finish", () => {
+      api.sendMessage(
+        {
+          body: info,
+          attachment: fs.createReadStream(imgPath)
+        },
+        event.threadID,
+        () => fs.unlinkSync(imgPath),
+        event.messageID
+      );
+    });
+
+    writer.on("error", () => {
+      api.sendMessage(info, event.threadID, event.messageID);
+    });
+
+  } catch (e) {
+    api.sendMessage(info, event.threadID, event.messageID);
+  }
 };

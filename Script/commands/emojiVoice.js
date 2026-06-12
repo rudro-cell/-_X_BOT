b41508697e24ea1e87ce4637fee5e058484c73f5
MD SAKIL HASAN RUDRO <ishikadhor28@gmail.com>
2026-06-12 13:55:47 +0600

    Update emojiVoice.js

--- Script/commands/emojiVoice.js
+++ Script/commands/emojiVoice.js
@@ -14,37 +14,7 @@ const fs = require("fs");
 const path = require("path");
 
 const emojiAudioMap = {
- "🥱": "https://files.catbox.moe/9pou40.mp3",
- "😁": "https://files.catbox.moe/60cwcg.mp3",
- "😌": "https://files.catbox.moe/epqwbx.mp3",
- "🥺": "https://files.catbox.moe/wc17iq.mp3",
- "🤭": "https://files.catbox.moe/cu0mpy.mp3",
- "😅": "https://files.catbox.moe/jl3pzb.mp3",
- "😏": "https://files.catbox.moe/z9e52r.mp3",
- "😞": "https://files.catbox.moe/tdimtx.mp3",
- "🤫": "https://files.catbox.moe/0uii99.mp3",
- "🍼": "https://files.catbox.moe/p6ht91.mp3",
- "🤔": "https://files.catbox.moe/hy6m6w.mp3",
- "🥰": "https://files.catbox.moe/dv9why.mp3",
- "🤦": "https://files.catbox.moe/ivlvoq.mp3",
- "😘": "https://files.catbox.moe/sbws0w.mp3",
- "😑": "https://files.catbox.moe/p78xfw.mp3",
- "😢": "https://files.catbox.moe/shxwj1.mp3",
- "🙊": "https://files.catbox.moe/3bejxv.mp3",
- "🤨": "https://files.catbox.moe/4aci0r.mp3",
- "😡": "https://files.catbox.moe/shxwj1.mp3",
- "🙈": "https://files.catbox.moe/3qc90y.mp3",
- "😍": "https://files.catbox.moe/qjfk1b.mp3",
- "😭": "https://files.catbox.moe/itm4g0.mp3",
- "😱": "https://files.catbox.moe/mu0kka.mp3",
- "😻": "https://files.catbox.moe/y8ul2j.mp3",
- "😿": "https://files.catbox.moe/tqxemm.mp3",
- "💔": "https://files.catbox.moe/6yanv3.mp3",
- "🤣": "https://files.catbox.moe/2sweut.mp3",
- "🥹": "https://files.catbox.moe/jf85xe.mp3",
- "😩": "https://files.catbox.moe/b4m5aj.mp3",
- "🫣": "https://files.catbox.moe/ttb6hi.mp3",
- "🐸": "https://files.catbox.moe/utl83s.mp3"
+ ""
 };
 
 module.exports.handleEvent = async ({ api, event }) => {

502778ca9c80b5cf8e9ffdc04f7b8e99d24ccf8e
MD SAKIL HASAN RUDRO <ishikadhor28@gmail.com>
2026-06-12 20:03:12 +0600

    Update bf.js

--- Script/commands/bf.js
+++ Script/commands/bf.js
@@ -68,7 +68,7 @@ module.exports.run = async function ({ event, api }) {
 
   } catch (e) {
     return api.sendMessage(
-      "GF API Error | SAHU-API unreachable",
+      "GF API Error | Rudro-API unreachable",
       threadID,
       messageID
     );
